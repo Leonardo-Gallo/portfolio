1e9f46ef6ad7fdda9ce289b8043065ca02e5635a
Leonardo Gallo <119631187+Leonardo-Gallo@users.noreply.github.com>
2024-05-03 00:06:42 +0000

    button functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
 					<div>
 						<div className="flex flex-wrap justify-center gap-2">
 							<a 
-								href="www.linkedin.com/in/leonardo-gallo-41835914a"
+								href="https://www.linkedin.com/in/leonardo-gallo-41835914a"
 								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 								<svg
 								className="w-5 h-5 fill-current"
