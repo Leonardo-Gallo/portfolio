28f119508a98d4c34321604727e664eff8188270
Leonardo Gallo <119631187+Leonardo-Gallo@users.noreply.github.com>
2025-07-03 01:11:14 +0000

    Edit's timeline

--- src/components/data/timeline.js
+++ src/components/data/timeline.js
@@ -1,15 +1,9 @@
 export default [
     {
         year:'2025',
-        title:'Studying for CompTIA A+ Certification',
+        title:'Taking IBM\'s Introduction to Software Engineering on Coursera',
         duration:'Now',
-        details:'To kick open the door to IT success, I\'m currently tackling this certification.'
-    },
-    {
-        year:'2025',
-        title:'Software Engineering Student at Western Governers University',
-        duration:'5 Months',
-        details:'I\'ve decided to continue my education in order to open up more opportunities in the job market and to deepen my knowledge in software engineering. Due to life, I ended my education there for now.'
+        details:'To kick open the door to IT success, I\'m currently tackling this professional certification.'
     },
     {
         year:'2024',
